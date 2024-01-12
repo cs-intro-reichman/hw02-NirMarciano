119befaae1cbc8daec13374efd02b8170c19fbc2
dorSk12 <154928993+dorSk12@users.noreply.github.com>
2024-01-12 11:38:03 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -2,10 +2,11 @@ public class DamkaBoard {
 	public static void main (String[] args) {
 
 		int n = Integer.parseInt(args[0]);
+		// # feedback: Consider initializing 'i' and 'j' within the for loops to improve code clarity and prevent unintentional reuse of loop variables outside their intended scope.
 		int i,j;
 
 		for(i = 1; i <= n ; i++) {
-
+			// # Feedback: Your code structure is well-organized and effectively prints an n-by-n damka board. Good job!
 			if(i % 2 == 0) {
 				System.out.print(" ");
 			}
