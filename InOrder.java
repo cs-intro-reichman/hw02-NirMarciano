e6f20aa788a558cb7d0515643e21c8b7fe027cf4
dorSk12 <154928993+dorSk12@users.noreply.github.com>
2024-01-12 11:41:29 +0200

    Update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -1,11 +1,11 @@
 public class InOrder {
 	public static void main (String[] args) {
-
+		// # Feedback: Consider using a do-while loop to eliminate the need for initializing x and y outside the loop.
 		int x = -1;
 		int y = -2;
 		
 		while(x >= y) {
- 			
+ 			// # Feedback: Consider removing extra spacing between lines to improve code aesthetics.
  			y = x;
 			x = (int) ((Math.random() * 11) - 1);
 			if (x >= y) {
